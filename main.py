15337579d3a21874a275dbb684ce468e4492bbc4
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2026-01-21 00:03:36 +0000

    Move benching function to core.py

--- main.py
+++ main.py
@@ -4,39 +4,12 @@ import platform
 import subprocess
 import time
 
-from yaspin import yaspin
-from yaspin.spinners import Spinners
-
 from exceptions import *
 
 args = sys.argv
 
 if platform.system() == "Windows" and '--allow-windows' not in args:
     raise PlatformNotSupportedError("Windows cannot possibly have coreutils installed and is not a supported configuration. Even if it does, it is under Cygwin and has way too much overhead to be accurate. Please install Linux or another UNIX-like system on your computer to continue.")
-
-def run_bench(mode):
-    global spinner
-
-    if mode == "time":
-        print("Running time benchmark for 30 seconds...")
-        result_30sec = 0
-        process = subprocess.Popen(['yes'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        end = time.time() + 30
-        with yaspin(Spinners.line):
-            while time.time() < end:
-                line = process.stdout.readline()
-                result_30sec += 1
-        process.terminate()
-        print("Running time benchmark for 60 seconds...")
-        result_60sec = 0
-        process = subprocess.Popen(['yes'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        end = time.time() + 60
-        with yaspin(Spinners.line):
-            while time.time() < end:
-                line = process.stdout.readline()
-                result_60sec += 1
-        process.terminate()
-        return result_30sec, result_60sec
     
 def main():
     if '--help' in args or '-h' in args:
