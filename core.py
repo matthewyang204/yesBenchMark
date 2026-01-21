1fc3d18d8c19a71c610b5db334134f326ecf49c0
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2026-01-21 00:07:49 +0000

    Make benching system modular

--- core.py
+++ core.py
@@ -4,26 +4,30 @@ import time
 from yaspin import yaspin
 from yaspin.spinners import Spinners
 
+def run_timed_bench():
+    print("Running time benchmark for 30 seconds...")
+    result_30sec = 0
+    process = subprocess.Popen(['yes'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    end = time.time() + 30
+    with yaspin(Spinners.line):
+        while time.time() < end:
+            line = process.stdout.readline()
+            result_30sec += 1
+    process.terminate()
+    print("Running time benchmark for 60 seconds...")
+    result_60sec = 0
+    process = subprocess.Popen(['yes'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    end = time.time() + 60
+    with yaspin(Spinners.line):
+        while time.time() < end:
+            line = process.stdout.readline()
+            result_60sec += 1
+    process.terminate()
+    return result_30sec, result_60sec
+
+
 def run_bench(mode):
     global spinner
 
     if mode == "time":
-        print("Running time benchmark for 30 seconds...")
-        result_30sec = 0
-        process = subprocess.Popen(['yes'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        end = time.time() + 30
-        with yaspin(Spinners.line):
-            while time.time() < end:
-                line = process.stdout.readline()
-                result_30sec += 1
-        process.terminate()
-        print("Running time benchmark for 60 seconds...")
-        result_60sec = 0
-        process = subprocess.Popen(['yes'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        end = time.time() + 60
-        with yaspin(Spinners.line):
-            while time.time() < end:
-                line = process.stdout.readline()
-                result_60sec += 1
-        process.terminate()
-        return result_30sec, result_60sec
+        run_timed_bench()
