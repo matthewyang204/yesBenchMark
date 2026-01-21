96ace77d126e45b1216bcd04d849f31359edb3f1
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2026-01-21 03:17:59 +0000

    Add function for getting processor frequency in mhz

--- resources.py
+++ resources.py
@@ -12,3 +12,11 @@ def multirun(func, n):
             y.append(b)
 
     return x, y
+
+def get_proc_mhz():
+    freqs = []
+    with open("/proc/cpuinfo") as f:
+        for line in f:
+            if "cpu MHz" in line:
+                freqs.append(float(line.split(":")[1].strip()))
+    return freqs
