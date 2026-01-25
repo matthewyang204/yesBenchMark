96dc1c5efbdf839debd2f173cd086827310b6a4b
Matthew Yang <matthew.yang204@gmail.com>
2026-01-24 18:40:19 -0800

    Fix stress benchmark on PowerPC OS X

--- yesbenchmark/stress.py
+++ yesbenchmark/stress.py
@@ -1,6 +1,13 @@
 import subprocess
 import time
-import psutil
+import platform
+
+# Troublesome imports
+try:
+    import psutil
+    psutilExist = True
+except ImportError:
+    psutilExist = False
 
 from yaspin import yaspin
 from yaspin.spinners import Spinners
@@ -8,17 +15,23 @@ from yaspin.spinners import Spinners
 from .resources import *
 from .freq import *
 from .exceptions import *
+from .darwin_shenanigans import *
 
 def run_stress_bench():
     spinner = yaspin(Spinners.line)
+    if platform.system() == "Darwin" and psutilExist == False:
+        cpuPfunc = cpu_percent_macos
+    else:
+        cpuPfunc = psutil.cpu_percent
     print("Running stress benchmark for 30 seconds...")
     result_30sec = []
     process = subprocess.Popen(['yes'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     end = time.time() + 30
     spinner.start()
-    nothingInParticular = psutil.cpu_percent(interval=None) # initialize psutil
+    if psutilExist:
+        nothingInParticular = cpuPfunc(interval=None) # initialize psutil
     while time.time() < end:
-        result_30sec.append(psutil.cpu_percent(interval=1))
+        result_30sec.append(cpuPfunc(interval=1))
     spinner.stop()
     process.terminate()
     print("Running stress benchmark for 60 seconds...")
@@ -26,9 +39,10 @@ def run_stress_bench():
     process = subprocess.Popen(['yes'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     end = time.time() + 60
     spinner.start()
-    nothingInParticular = psutil.cpu_percent(interval=None) # initialize psutil again
+    if psutilExist:
+        nothingInParticular = cpuPfunc(interval=None) # initialize psutil again
     while time.time() < end:
-        result_60sec.append(psutil.cpu_percent(interval=1))
+        result_60sec.append(cpuPfunc(interval=1))
     spinner.stop()
     process.terminate()
     return result_30sec, result_60sec
